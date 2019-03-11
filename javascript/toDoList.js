8b9a3cfaa8371e9854999e803304b04b3852f840
feliciawallin <felicia.e.wallin@gmail.com>
2019-03-11 13:00:46 +0100

    Commit to-do-list project

---
+++ javascript/toDoList.js
@@ -0,0 +1,115 @@
+var myToDoArray = [];
+var doneArray = [];
+
+function init() {
+
+    // Kollar om min localstorage är tom eller null då läggs min lista i localstorage annars hämta det som finns i localstorage.
+    if (localStorage.getItem("myStoredToDoArray") == null || localStorage.getItem("myStoredToDoArray") == "[]") {
+
+         myToDoArray = ["Take André to the doctor", "Meet Alexa", "Have a strawberrydaq", "watch the last episode of Riverdale", "By a new dress"];
+        localStorage.setItem("myStoredToDoArray", JSON.stringify(myToDoArray));
+
+    } else {
+        var myToDoArray = JSON.parse(localStorage.getItem('myStoredToDoArray'));
+    }
+
+    // Om det finns något i min localstorage hämta värderna och spara i en variabel.
+    if (localStorage.getItem("myStoredDoneArray")) {
+         doneArray = JSON.parse(localStorage.getItem('myStoredDoneArray'));
+    }
+
+    printToDo();
+}
+
+
+function printToDo() {
+    // Hämtar myToDoArray från Localstorage
+    var getMyToDoArray = localStorage.getItem("myStoredToDoArray");
+
+    myToDoArray = JSON.parse(getMyToDoArray);
+
+
+
+    // Hämtar doneArray från Localstorage
+    var getMyDoneArray = localStorage.getItem("myStoredDoneArray");
+
+    doneArray = JSON.parse(getMyDoneArray);
+
+
+
+    // skriver ut myToDoArray 
+    var myFullPrintedToDo = " ";
+
+    for (var i = 0; i < myToDoArray.length; i++) {
+        myFullPrintedToDo += "<li class='row justify-content-center'>" + "<div class='col-4 text-left'><p class='upperCase'>" + " " + myToDoArray[i] + "</p></div>" + '<div class="col-4 text-right"><button id="' + i + '" onclick="deleteToDo(' + i + ')"><i class="far fa-trash-alt"></i></button>' + "<input onclick='moveToDone(" + i + ")' type='checkbox' id='" + i + "'></div></li>";
+    }
+
+    document.getElementById("printingMyArray").innerHTML = myFullPrintedToDo;
+
+    // skriver ut doneArray
+    var myFullPrintedToDone = " ";
+
+    for (var i = 0; i < doneArray.length; i++) {
+        myFullPrintedToDone += "<li class='row justify-content-center'>" + "<div class='col-4 text-left'><p class='lineThrough upperCase'>" + " " + doneArray[i] + "</p></div>" + '<div class="col-4 text-right"><button id="' + i + '"onclick="deleteDone(' + i + ')"><i class="far fa-trash-alt"></i></button>' + "<input onclick='moveToToDo(" + i + ")' type='checkbox' checked id='" + i + "'></div></li>";
+    }
+
+    document.getElementById("printDoneArray").innerHTML = myFullPrintedToDone;
+}
+
+function addItem() {
+    var inputValue = document.getElementById("addedItemValue").value;
+    myToDoArray.push(inputValue);
+    save();
+    printToDo();
+}
+
+function moveToDone(id) {
+
+    var moveThis = myToDoArray.splice(id, 1);
+    var turnToString = moveThis.toString();
+    doneArray.push(turnToString);
+    save();
+    printToDo();
+}
+
+function moveToToDo(id) {
+    var moveThis = doneArray.splice(id, 1);
+    var turnToString = moveThis.toString();
+    myToDoArray.push(turnToString);
+    save();
+    printToDo();
+}
+
+function sortMyList() {
+    myToDoArray.sort(function(x,y) {
+        var a = x.toLower();
+        var b = y.toLower();
+
+        if (a < b) {
+            return -1;
+        }
+        if(a > b) {
+            return 1;
+        }
+        return 0;
+    });
+    save();
+    printToDo();
+}
+
+function deleteToDo(position) {
+    myToDoArray.splice(position, 1);
+    save();
+    printToDo();
+}
+
+function deleteDone(position) {
+    doneArray.splice(position, 1);
+    save();
+    printToDo();
+}
+
+function save() {
+    localStorage.setItem("myStoredToDoArray", JSON.stringify(myToDoArray));
+    localStorage.setItem("myStoredDoneArray", JSON.stringify(doneArray));
+}
